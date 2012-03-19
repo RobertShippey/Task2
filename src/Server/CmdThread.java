898ef2c4a98e40e8148fd8ef44b175637eeb5f1c
Robert Shippey <robert@robertshippey.net>
2012-03-19 21:31:32 +0000

    add log off

--- src/Server/CmdThread.java
+++ src/Server/CmdThread.java
@@ -4,7 +4,6 @@
  */
 package Server;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
--- src/Server/Session.java
+++ src/Server/Session.java
@@ -4,7 +4,6 @@
  */
 package Server;
 
-
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -16,65 +15,75 @@ import shared.Booking;
  *
  * @author Robert
  */
-class Session extends Thread{
+class Session extends Thread {
+
     private Server server;
     private String _name;
     private Socket _ip;
     private ObjectInputStream in;
     private ObjectOutputStream out;
     private List<Booking> reservations;
-    
-    public Session (Socket ip, Server s) throws IOException{
+
+    public Session(Socket ip, Server s) throws IOException {
         server = s;
         _ip = ip;
         in = new ObjectInputStream(_ip.getInputStream());
-        out = new ObjectOutputStream(ip.getOutputStream());
-        
+        out = new ObjectOutputStream(_ip.getOutputStream());
+
     }
-    
+
     @Override
-    synchronized public void run(){
+    synchronized public void run() {
         try {
-            _name = (String)in.readObject();
+            _name = (String) in.readObject();
         } catch (IOException ex) {
             //could not connect through socket anymore
         } catch (ClassNotFoundException ex) {
             //Didn't send a String of their username to start
         }
-        
-        while(!server.quitting()){
-            try{
-            Object something = in.readObject();
-            //processing
-            
-            //if the client logs off
-            //server.removeClient(this); //then return I guess...
-            
-            out.writeObject(something);
+
+        while (!server.quitting()) {
+            try {
+                Object something = in.readObject();
+
+
+                //processing
+
+                if (something instanceof String) {
+                    String command = (String)something;
+                    if(command.equalsIgnoreCase("log off"))
+                    {
+                        server.removeClient(this);
+                        return;
+                    }
+                }
+
+                out.writeObject(something);
             } catch (IOException e) {
                 //could not connect though socket anymore
             } catch (ClassNotFoundException cnf) {
                 //client sent something weird
             }
         }
-        
-        
-        
+
+
+
     }
-    
-    public boolean isConnected(){
+
+    public boolean isConnected() {
         return _ip.isConnected();
 
     }
-    
-    public void forceQuit(){
-        try{
-        _ip.close();}
-        catch (IOException e){ }
+
+    public void forceQuit() {
+        try {
+            _ip.close();
+        } catch (IOException e) {
+        }
         _ip = null;
     }
-    
-    private void getCustomersReservations(){
+
+    private void getCustomersReservations() {
         reservations = server.findBookings(_name);
     }
 }
