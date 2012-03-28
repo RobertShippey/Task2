87931f65590f3b3caee3b7c6fed7c49f37483da9
Robert Shippey <robert@robertshippey.net>
2012-03-28 16:26:04 +0100

    reformat stuffs closes #8

--- src/Server/Data.java
+++ src/Server/Data.java
@@ -15,97 +15,100 @@ import shared.Film;
  * @author Robert
  */
 public class Data {
+
     private LinkedList<Film> films;
     private LinkedList<Booking> reservations;
-    
-    public Data(LinkedList<Film> films, LinkedList<Booking> reservations){
+
+    public Data(LinkedList<Film> films, LinkedList<Booking> reservations) {
         this.films = (LinkedList<Film>) Collections.synchronizedList(films);
         this.reservations = (LinkedList<Booking>) Collections.synchronizedList(reservations);
     }
-    
-    public Booking[] getReservations(String customerName){
-        synchronized(reservations){
-        LinkedList<Booking> r = new LinkedList<Booking>();
-        Iterator<Booking> it = reservations.iterator();
-        while(it.hasNext()){
-            if(it.next().getName().equals(customerName)){
-                r.add(it.next());
+
+    public Booking[] getReservations(String customerName) {
+        synchronized (reservations) {
+            LinkedList<Booking> r = new LinkedList<Booking>();
+            Iterator<Booking> it = reservations.iterator();
+            while (it.hasNext()) {
+                if (it.next().getName().equals(customerName)) {
+                    r.add(it.next());
+                }
             }
-        }
-        return (Booking[])r.toArray();
+            return (Booking[]) r.toArray();
         }
     }
-    
-    public Film findFilm(String name, String time){
-        synchronized(films){
-        Iterator<Film> fit = films.iterator();
-        while (fit.hasNext()) {
-            if (fit.next().getName().equals(name) && fit.next().getDate().equals(time)) {
-                return fit.next();
+
+    public Film findFilm(String name, String time) {
+        synchronized (films) {
+            Iterator<Film> fit = films.iterator();
+            while (fit.hasNext()) {
+                if (fit.next().getName().equals(name) && fit.next().getDate().equals(time)) {
+                    return fit.next();
+                }
             }
-        }
         }
         return null;
     }
-    
+
     public boolean makeReservation(String customer, String film, String time, int no) {
         Film f = findFilm(film, time);
         if (f == null) {
             return false;
         }
-        synchronized(reservations){
-        if (f.space() >= no) {
-            f.book(no);
-            Booking b = new Booking(customer, f, no);
-            reservations.add(b);
-            return true;
-        }
+        synchronized (reservations) {
+            if (f.space() >= no) {
+                f.book(no);
+                Booking b = new Booking(customer, f, no);
+                reservations.add(b);
+                return true;
+            }
         }
         return false;
     }
-    
-    public void cancelReservation(String customer, String film, String time, int no){
+
+    public void cancelReservation(String customer, String film, String time, int no) {
         Film f = findFilm(film, time);
         Booking[] b = getReservations(customer);
-        synchronized(reservations){
-        for(int x=0; x<b.length;x++){
-            if(b[x].getFilm() == f){
-                reservations.remove(b[x]);
+        synchronized (reservations) {
+            for (int x = 0; x < b.length; x++) {
+                if (b[x].getFilm() == f) {
+                    reservations.remove(b[x]);
+                }
+
             }
-            
-        }}
+        }
         f.free(no);
-        
+
     }
-    
-    public boolean changeReservation(String customer, String film, String time, int oldSeats, int newSeats){
+
+    public boolean changeReservation(String customer, String film, String time, int oldSeats, int newSeats) {
         this.cancelReservation(customer, film, time, oldSeats);
         return this.makeReservation(customer, film, time, newSeats);
     }
-    
-    public boolean isFull(String film, String time){
-        Film f = findFilm(film,time);
-        if(f.space()==0){
+
+    public boolean isFull(String film, String time) {
+        Film f = findFilm(film, time);
+        if (f.space() == 0) {
             return true;
-        } else { return false;}
-        
+        } else {
+            return false;
+        }
+
     }
-    
-    public int getSpace(String film, String time){
+
+    public int getSpace(String film, String time) {
         Film f = findFilm(film, time);
         return f.space();
     }
-    
-    public Iterator<Film> getFilmItt(){
-        synchronized(films){
-        return films.iterator();
+
+    public Iterator<Film> getFilmItt() {
+        synchronized (films) {
+            return films.iterator();
         }
     }
-    
-    public Iterator<Booking> getBookingItt(){
-        synchronized(reservations){
-        return reservations.iterator();
+
+    public Iterator<Booking> getBookingItt() {
+        synchronized (reservations) {
+            return reservations.iterator();
+        }
     }
-    }
-    
 }
