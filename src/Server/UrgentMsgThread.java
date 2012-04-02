17f4ac9e16910bdf43493210b65aa76b219ec808
Robert Shippey <robert@robertshippey.net>
2012-04-02 21:18:42 +0100

    seems to be working

--- src/Server/UrgentMsgThread.java
+++ src/Server/UrgentMsgThread.java
@@ -4,6 +4,9 @@
  */
 package Server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
@@ -13,25 +16,66 @@ import java.util.List;
  * @author Robert
  */
 class UrgentMsgThread extends Thread {
-    private Server _s;
-    private Data data;
-    private List<String> _msgs;
+
+    private Server server;
+    private ServerSocket s;
+    private List<Socket> clients;
 
     public UrgentMsgThread(Server server) {
-        _s = server;
-        data = _s.getData();
-        _msgs = Collections.synchronizedList(new LinkedList<String>());
+        this.server = server;
+        try {
+            s = new ServerSocket(2001);
+            s.setSoTimeout(Server.TIMEOUT);
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+        clients = Collections.synchronizedList(new LinkedList<Socket>());
     }
-    
+
     @Override
-    synchronized public void run(){
-        
+    synchronized public void run() {
+        if (s == null) {
+            return;
+        }
+        while (!server.quitting()) {
+            Socket cl = null;
+            try {
+                cl = s.accept();
+            } catch (IOException e) {
+                continue;
+            }
+            clients.add(cl);
+        }
+
     }
-    
-    public void send(String msg){
-        synchronized(_msgs){
-        _msgs.add(msg);
-    }}
-    
-    
+
+    public void send(String msg) {
+        synchronized (clients) {
+            UMSender m = new UMSender((Socket[]) clients.toArray(), msg);
+            m.start();
+        }
+    }
+
+    class UMSender extends Thread {
+
+        private Socket[] clients;
+        private String message;
+
+        public UMSender(Socket[] c, String m) {
+            clients = c;
+            message = m;
+        }
+
+        @Override
+        public void run() {
+            for (int x = 0; x < clients.length; x++) {
+                try {
+                    clients[x].getOutputStream().write(message.getBytes());
+                } catch (IOException e) {
+                    continue;
+                }
+            }
+
+        }
+    }
 }
